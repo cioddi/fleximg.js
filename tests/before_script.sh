829e497024f965f3d30bab8893e1021630d2716f
cioddi <maxtobiasweber@gmail.com>
2013-08-18 20:19:35 +0200

    fix test

--- tests/before_script.sh
+++ tests/before_script.sh
@@ -11,7 +11,7 @@ sudo sed -i -e "s,AllowOverride[ ]None,AllowOverride All,g" /etc/apache2/sites-a
 
 sudo /etc/init.d/apache2 restart
 
-echo "<?php php_info(); ?>" > info.php
+echo "<?php phpinfo(); ?>" > info.php
 lynx -dump "http://localhost/info.php"
 
 cat /etc/apache2/sites-available/default
