3e6839fb0fb3a2f86fb734f08764ad32957ccb03
cioddi <maxtobiasweber@gmail.com>
2013-08-18 21:10:45 +0200

    fix test

--- tests/before_script.sh
+++ tests/before_script.sh
@@ -1,5 +1,6 @@
+echo "Install and setup apache"
 sudo apt-get update > /dev/null
-sudo apt-get install -y --force-yes apache2 libapache2-mod-php5 lynx php5-curl php5-intl php5-curl php5-gd php5-idn php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl
+sudo apt-get install -y --force-yes apache2 libapache2-mod-php5 php5-curl php5-intl php5-curl php5-gd php5-idn php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl
 
 sudo a2enmod rewrite
 chmod 777 /home/travis/build/cioddi/fleximg.js/img/fleximg_scale
@@ -7,14 +8,9 @@ chmod 777 /home/travis/build/cioddi/fleximg.js/img/fleximg_scale
 sudo sed -i -e "s,/var/www,/home/travis/build/cioddi/fleximg.js,g" /etc/apache2/sites-available/default
 sudo sed -i -e "s,AllowOverride[ ]None,AllowOverride All,g" /etc/apache2/sites-available/default
 
-
-
 sudo /etc/init.d/apache2 restart
 
-echo "<?php phpinfo(); ?>" > info.php
-lynx -dump "http://localhost/info.php"
 
-cat /etc/apache2/sites-available/default
 serverUrl='http://127.0.0.1:4444'
 serverFile=selenium-server-standalone-2.35.0.jar
 firefoxUrl=http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/21.0/linux-x86_64/en-US/firefox-21.0.tar.bz2
@@ -27,6 +23,11 @@ pear install phpunit/PHP_Invoker
 pear install phpunit/DbUnit
 pear install phpunit/PHPUnit_Selenium
 pear install phpunit/PHPUnit_Story
+
+echo "Install Bower dependencies"
+npm install -g bower
+bower install
+
 
 echo "Downloading Firefox"
 wget $firefoxUrl -O $firefoxFile
@@ -46,8 +47,3 @@ else
     echo "Finished setup"
 fi
 
-
-npm install -g bower
-bower install
-
-ls -la ./
