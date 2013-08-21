e32b12a12311e3561a6fba9ce848979dde66e98b
cioddi <maxtobiasweber@gmail.com>
2013-08-21 17:38:43 +0200

    saucelab travis integration

--- tests/saucelab_test.sh
+++ tests/saucelab_test.sh
@@ -9,9 +9,7 @@ cd $CONNECT_DIR
 curl $CONNECT_URL > $CONNECT_DOWNLOAD
 unzip $CONNECT_DOWNLOAD
 rm $CONNECT_DOWNLOAD
-java -jar Sauce-Connect.jar --readyfile $READY_FILE \
-    --tunnel-identifier $TRAVIS_JOB_NUMBER \
-    $SAUCE_USERNAME $SAUCE_ACCESS_KEY &
+java -jar Sauce-Connect.jar $SAUCE_USERNAME $SAUCE_ACCESS_KEY --readyfile $READY_FILE --tunnel-identifier $TRAVIS_JOB_NUMBER &
 
 # Wait for Connect to be ready before exiting
 while [ ! -f $READY_FILE ]; do
